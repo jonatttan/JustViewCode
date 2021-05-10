1364317a8e079ea430a04e1aed24c50a41523525
Jonattan <jonattan.sousa@gmail.com>
2021-05-10 10:52:25 -0300

    Efetuado instalação do SnapKit para facilitar aplicação de constraints, o que foi exemplificado co o botão desenvolvido até o momento. Este commit está com comentários, que serão removidos no próximo.

--- JustViewCode/ViewControllerScreen.swift
+++ JustViewCode/ViewControllerScreen.swift
@@ -13,7 +13,7 @@ final class ViewControllerScreen: UIView {
         let view = UIButton(frame: .zero)
         view.backgroundColor = .red
         view.setTitle("fetch", for: .normal)
-        view.translatesAutoresizingMaskIntoConstraints = false
+//        view.translatesAutoresizingMaskIntoConstraints = false // Em desuso com SnapKit
         return view
     }()
     
@@ -35,10 +35,17 @@ extension ViewControllerScreen: CodeView {
     func setupConstraints() {
         // Configurar constraints
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
-        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
-        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15)    .isActive = true
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+//        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+//        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
+//        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15)    .isActive = true
+//        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        button.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().inset(15)   // .offset(-15) // Em vez de usar negativo no offset, usamos inset
+            make.bottom.equalTo(self).inset(15) // .equalToSuperview().inset(15) // EmVez de declarar Superview, podemos utilizar .equalTo(self)
+            make.height.equalTo(50)
+        }
     }
     func setupAdditionalConfiguration() {
         // Setup adicional
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'JustViewCode' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for JustViewCode
+
+  target 'JustViewCodeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'JustViewCodeUITests' do
+    # Pods for testing
+  end
+  
+  pod 'SnapKit', '~> 5.0.0'
+
+end
