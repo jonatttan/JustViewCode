49cd891625ed432a3a5fac81461319e5d86a86be
Jonattan <jonattan.sousa@gmail.com>
2021-05-10 10:55:41 -0300

    Eliminado comentários do commit anterior, onde foi exemplificado o uso do SnapKit e as linhas defasadas de aplicação de constraint foram comentadas.

--- JustViewCode/ViewControllerScreen.swift
+++ JustViewCode/ViewControllerScreen.swift
@@ -9,19 +9,16 @@ import UIKit
 
 final class ViewControllerScreen: UIView {
     
-    lazy var button: UIButton = { // Clouse de criação. Só é possivel chamada de método dentro se for lazy
+    lazy var button: UIButton = {
         let view = UIButton(frame: .zero)
         view.backgroundColor = .red
         view.setTitle("fetch", for: .normal)
-//        view.translatesAutoresizingMaskIntoConstraints = false // Em desuso com SnapKit
         return view
     }()
-    
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
         setupView()
     }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -35,15 +32,11 @@ extension ViewControllerScreen: CodeView {
     func setupConstraints() {
         // Configurar constraints
         button.translatesAutoresizingMaskIntoConstraints = false
-//        button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
-//        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
-//        button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -15)    .isActive = true
-//        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
         button.snp.makeConstraints { make in
             make.left.equalToSuperview().offset(15)
-            make.right.equalToSuperview().inset(15)   // .offset(-15) // Em vez de usar negativo no offset, usamos inset
-            make.bottom.equalTo(self).inset(15) // .equalToSuperview().inset(15) // EmVez de declarar Superview, podemos utilizar .equalTo(self)
+            make.right.equalToSuperview().inset(15)
+            make.bottom.equalTo(self).inset(15)
             make.height.equalTo(50)
         }
     }
